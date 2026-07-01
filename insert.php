4ab549030411d8e8f59d0183d96ea0dcfcdfcd59
corwinteo-45 <corwint45@gmail.com>
2026-07-01 15:36:53 +0800

    insert.php

--- insert.php
+++ insert.php
@@ -8,9 +8,8 @@
         require "database/config.php";
 
         //Establish the connection
-        $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+        $conn = mysqli_init()
+        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306)){
             die('Failed to connect to MySQL: '.mysqli_connect_error());
         }
 
