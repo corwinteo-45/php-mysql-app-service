ea8d7ac7646135ff2375b3c85dc930fe1e3c6398
corwinteo-45 <corwint45@gmail.com>
2026-07-01 15:37:41 +0800

    delete.php

--- delete.php
+++ delete.php
@@ -9,8 +9,7 @@
         require "database/config.php";
         //Establish the connection
         $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306)){
             die('Failed to connect to MySQL: '.mysqli_connect_error());
         }
 
